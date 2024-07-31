3362f74feeca287cb5edc64ea165ecda3da4c086
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-07-30 22:24:41 -0300

    feat: primeiro estudinho

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Esse é o Screen Match");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 9.0;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.printf("%.2f\n", media);
+        System.out.println("""
+                Relembrando as coisas
+                    Relembrando as coisas
+                        Relembrando as coisas
+                """);
+
+        int classificacao;
+        classificacao = (int) (media / 2);
+        System.out.println(classificacao);
     }
 }
