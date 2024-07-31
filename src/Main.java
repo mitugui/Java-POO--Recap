467f0bc8b8fbe1c43604dcdff88c5f90d6876354
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-07-31 20:51:31 -0300

    feat: modelando a classe Film

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esse é o Screen Match");
+        Film myFilm = new Film();
+        myFilm.setName("Godzilla Minus One");
 
-        int anoDeLancamento = 2022;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        boolean incluidoNoPlano = true;
-        double notaDoFilme = 9.0;
-
-        double media = (9.8 + 6.3 + 8.0) / 3;
-        System.out.printf("%.2f\n", media);
-        System.out.println("""
-                Relembrando as coisas
-                    Relembrando as coisas
-                        Relembrando as coisas
-                """);
-
-        int classificacao;
-        classificacao = (int) (media / 2);
-        System.out.println(classificacao);
+        System.out.println(myFilm.getName());
     }
 }
