ad09045f84ea5668c3da77191b985e80a2891778
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-07-31 21:26:21 -0300

    refactor: removi getters e setters indesejados

---
+++ src/Film.java
@@ -0,0 +1,58 @@
+public class Film {
+    private String name;
+    private int releaseYear;
+    private boolean includedInPlan;
+    private double ratingSum;
+    private int totalRatings;
+    private int durationInMinutes;
+
+    public void displayTechnicalSheet () {
+        System.out.println("Nome do filme: " + name);
+        System.out.println("Ano de lançamento: " + releaseYear);
+    }
+
+    public void rate(double rating) {
+        ratingSum += rating;
+        totalRatings++;
+    }
+
+    public double getAverage() {
+        return ratingSum / totalRatings;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public boolean isIncludedInPlan() {
+        return includedInPlan;
+    }
+
+    public void setIncludedInPlan(boolean includedInPlan) {
+        this.includedInPlan = includedInPlan;
+    }
+
+    public int getTotalRatings() {
+        return totalRatings;
+    }
+
+    public int getDurationInMinutes() {
+        return durationInMinutes;
+    }
+
+    public void setDurationInMinutes(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+}
