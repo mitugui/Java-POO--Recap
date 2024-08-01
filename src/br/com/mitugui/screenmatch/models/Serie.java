95838f376ac3be45da9134231760e36bdbdb4de1
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-08-01 08:57:29 -0300

    feat: sobreescrevendo o método getDurationInMinutes em Serie

--- src/br/com/mitugui/screenmatch/models/Serie.java
+++ src/br/com/mitugui/screenmatch/models/Serie.java
@@ -37,4 +37,9 @@ public class Serie extends Title {
     public void setMinutesPerEpisode(int minutesPerEpisode) {
         this.minutesPerEpisode = minutesPerEpisode;
     }
+
+    @Override
+    public int getDurationInMinutes() {
+        return seasons * episodesPerSeason * minutesPerEpisode;
+    }
 }
