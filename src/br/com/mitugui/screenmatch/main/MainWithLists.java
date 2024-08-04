e535f23645241205ced16f801fab6ca431f1222e
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-08-04 11:44:41 -0300

    feat: criando um main para trabalhar com listas e iterando títulos em uma lista usando também instance of

---
+++ src/br/com/mitugui/screenmatch/main/MainWithLists.java
@@ -0,0 +1,51 @@
+package br.com.mitugui.screenmatch.main;
+
+import br.com.mitugui.screenmatch.models.Film;
+import br.com.mitugui.screenmatch.models.Series;
+import br.com.mitugui.screenmatch.models.Title;
+
+import java.util.ArrayList;
+
+public class MainWithLists {
+    public static void main(String[] args) {
+        Film favoriteFilm = new Film(
+                "Godzilla Minus One",
+                2023
+        );
+        favoriteFilm.rate(10);
+
+        Film otherFilm = new Film(
+                "The Boy and the Heron",
+                2023
+        );
+        otherFilm.rate(9);
+
+        Film newFilm = new Film(
+                "Swing Girls",
+                2004
+        );
+        newFilm.rate(10);
+
+        Series favoriteSeries = new Series(
+                "The Office",
+                2005,
+                9,
+                false,
+                22,
+                22
+        );
+
+        ArrayList<Title> watchedList = new ArrayList<>();
+        watchedList.add(favoriteFilm);
+        watchedList.add(otherFilm);
+        watchedList.add(newFilm);
+        watchedList.add(favoriteSeries);
+
+        for(Title title: watchedList){
+            System.out.println(title.getName());
+            if (title instanceof Film film) {
+                System.out.println("Classificação: " + film.getClassification());
+            }
+        }
+    }
+}
