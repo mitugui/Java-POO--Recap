08f8b71afac7e72ccf6d961eaf42fb172e9088e3
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-08-05 23:44:46 -0300

    feat: pedindo ao usuário o termo da pesquisa

--- src/br/com/mitugui/screenmatch/main/MainWithSearch.java
+++ src/br/com/mitugui/screenmatch/main/MainWithSearch.java
@@ -5,12 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite um filme para a busca: ");
+        var search = scan.nextLine();
+        String KEY = "";
+
+        String address = "http://www.omdbapi.com/?t=" + search + "&apikey=" + KEY;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(""))
+                .uri(URI.create(address))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
