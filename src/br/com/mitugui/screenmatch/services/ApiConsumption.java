9a4b30060b2478f6a537214dcc944b6ef54269d9
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-08-14 16:13:01 -0300

    refactor: movendo lógica de consumo de API para a classe ApiConsumption, com fim em generalizar o serviço, e atualizando QueryTitle

---
+++ src/br/com/mitugui/screenmatch/services/ApiConsumption.java
@@ -0,0 +1,20 @@
+package br.com.mitugui.screenmatch.services;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiConsumption {
+    String getData(String address) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(address))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+}
--- src/br/com/mitugui/screenmatch/services/QueryTitle.java
+++ src/br/com/mitugui/screenmatch/services/QueryTitle.java
@@ -7,28 +7,20 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class QueryTitle {
     public Title queryTitle(String address, Gson gson) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(address))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        var apiConsumption = new ApiConsumption();
+        String responseBody = apiConsumption.getData(address);
 
-        JsonObject jsonResponse = JsonParser.parseString(response.body()).getAsJsonObject();
+        JsonObject jsonResponse = JsonParser.parseString(responseBody).getAsJsonObject();
         if (jsonResponse.get("Response").getAsString().equals("False")) {
             System.out.println("Nenhum resultado encontrado. Tente novamente com outro filme.");
             return null;
         }
 
-        TitleOmdb titleOmdb = gson.fromJson(response.body(), TitleOmdb.class);
-        Title title = new Title(titleOmdb);
+        var titleOmdb = gson.fromJson(responseBody, TitleOmdb.class);
+        var title = new Title(titleOmdb);
         System.out.println("Resultado da pesquisa: " + title);
 
         return title;
